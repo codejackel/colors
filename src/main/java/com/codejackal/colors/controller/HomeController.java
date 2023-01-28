427aec266548121bb294931e228637ad77180fd3
Bhushan Padhye <94575939+bhushanpadhye@users.noreply.github.com>
2023-01-28 01:16:46 -0500

    Removed compilation error for unused import

--- src/main/java/com/codejackal/colors/controller/HomeController.java
+++ src/main/java/com/codejackal/colors/controller/HomeController.java
@@ -2,15 +2,12 @@ package com.codejackal.colors.controller;
 
 import com.codejackal.colors.domain.AppArgs;
 import lombok.extern.slf4j.Slf4j;
-import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -33,13 +30,13 @@ public class HomeController {
     private String id;
 
     @GetMapping
-    public ResponseEntity<Map<String, String>> getInfo() throws IOException, XmlPullParserException {
+    public ResponseEntity<Map<String, String>> getInfo() {
 
         Map<String, String> response = new HashMap<>();
-        response.put("id",id);
-        response.put("groupId",groupId);
-        response.put("artifactId",artifactId);
-        response.put("version",projectVersion);
+        response.put("id", id);
+        response.put("groupId", groupId);
+        response.put("artifactId", artifactId);
+        response.put("version", projectVersion);
         response.put("color", AppArgs.getColor());
         response.put("message", AppArgs.getMessage());
 
